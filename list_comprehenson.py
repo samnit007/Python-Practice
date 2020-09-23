7fec3d0465db5aa3c92c48c2d8d0cf357ad92482
Samnit Singh <singhsamnit2@gmail.com>
2020-09-23 13:34:47 +1000

    Final Update

--- list_comprehenson.py
+++ list_comprehenson.py
@@ -25,5 +25,3 @@ if __name__ == '__main__':
     n = int(input())
 
     print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)])
-    print("New commit")
-    print("New commit 2")
